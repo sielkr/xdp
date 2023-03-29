11c059669e21d818b52f7ffecd3a8b11983008ee
Siel <sielpersonal@icloud.com>
2023-03-29 21:21:39 +0900

    Update udpclient.py

--- src/test/udpclient.py
+++ src/test/udpclient.py
@@ -5,7 +5,7 @@ bytesToSend = str.encode(msgFromClient)
 serverAddressPort = ("127.0.0.1", 20001)
 bufferSize = 1024
 
-def udp_server():
+def udp_client():
   UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
   UDPClientSocket.sendto(bytesToSend, serverAddressPort)
   msgFromServer = UDPClientSocket.recvfrom(bufferSize)
